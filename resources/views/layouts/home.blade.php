97d0d9e08bda25143bc04f616d433b9a6fe2a341
bazimya <bazimyas@gmail.com>
2020-09-10 20:31:00 +0200

    preloard removed

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -25,7 +25,7 @@
 </head>
 <body>
 <!-- Preloader Start -->
-<div id="preloader-active">
+{{-- <div id="preloader-active">
     <div class="preloader d-flex align-items-center justify-content-center">
         <div class="preloader-inner position-relative">
             <div class="preloader-circle"></div>
@@ -34,7 +34,7 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 <!-- Preloader Start -->
 <header>
 <!-- Header Start -->
@@ -69,20 +69,20 @@
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
-                               
+
                             @else
                                 <li class="nav-item dropdown">
                                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                         {{ Auth::user()->name }}
                                     </a>
-    
+
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                         <a class="dropdown-item" href="{{ route('logout') }}"
                                            onclick="event.preventDefault();
                                                          document.getElementById('logout-form').submit();">
                                             {{ __('Logout') }}
                                         </a>
-    
+
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                             @csrf
                                         </form>

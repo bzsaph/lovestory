8df1a2f24079d2be63e5b5ac1d9dd41462356b4e
kijyamba <34911199+bzsaph@users.noreply.github.com>
2021-12-06 21:24:02 +0200

    style="margin-top:10% !important"

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.home')
 
 @section('content')
-<div class="container">
+<div class="container" style="margin-top:10% !important">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
